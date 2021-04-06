f6209abee64ba4232931fbbefbdbc582fb4261cc
joshua londono <joshualondono@outlook.com>
2021-04-06 15:33:54 -0400

    https://bezkoder.com/node-js-mongodb-auth-jwt/

--- app/models/index.js
+++ app/models/index.js
@@ -5,7 +5,11 @@ mongoose.Promise = global.Promise;
 
 const db = {};
 db.mongoose = mongoose;
+db.user = require("./user.model");
+db.role = require("./role.model");
 db.url = dbConfig.url;
 db.tutorials = require("./tutorial.model.js")(mongoose);
+
+db.ROLES = ["user", "admin", "moderator"];
 
 module.exports = db;
